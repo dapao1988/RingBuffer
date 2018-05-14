948a443fd85b41a73aa5482b6ab9d7700d5e2608
Wenbing.Wang <wenbing.wang@rokid.com>
2018-05-14 14:16:24 +0800

    add time start/end statistics, but thread2 is dead loop

--- RingBuffer.cpp
+++ RingBuffer.cpp
@@ -7,8 +7,10 @@
 #include "queue.hpp"
 #include <thread>
 #include <iostream>
+#include <chrono>
 
 using namespace std;
+using namespace chrono;
 
 void fun_generate(Queue<int> &queue)
 {
@@ -19,7 +21,7 @@ void fun_generate(Queue<int> &queue)
 		queue.enqueue(i);
 		cout<<"enqueue:"<<i<<"done"<<endl;
 	}
-	std::this_thread::sleep_for(std::chrono::seconds(1));
+	//std::this_thread::sleep_for(std::chrono::seconds(1));
 }
 
 void fun_consume(Queue<int> &queue)
@@ -32,7 +34,7 @@ void fun_consume(Queue<int> &queue)
 			std::shared_ptr< int > i = queue.dequeue();
 			cout<<"dequeue:"<<dec<<*(i.get())<<endl<<"use count:"<<dec<<i.use_count()<<endl;
 		}
-		std::this_thread::sleep_for(std::chrono::seconds(1));
+		//std::this_thread::sleep_for(std::chrono::seconds(1));
 	} while (true);
 	cout<<"exit fun_consume!"<<endl;
 }
@@ -40,9 +42,15 @@ void fun_consume(Queue<int> &queue)
 int main(int argn, const char* args[])
 {
 	Queue<int> queue(16);
+	auto start = system_clock::now();
 	std::thread t1(fun_generate, std::ref(queue));
 	std::thread t2(fun_consume, std::ref(queue));
 	t1.join();
 	t2.join();
+	auto end   = system_clock::now();
+	auto duration = duration_cast<microseconds>(end - start);
+	cout <<  "花费了"
+	     << double(duration.count()) * microseconds::period::num / microseconds::period::den
+	     << "秒" << endl;
 	return 0;
 }
